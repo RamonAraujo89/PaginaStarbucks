198a6ce38a96c4a77a2c7329e23c97a9e2b93390
RamonAraujo89 <ramon.araujo.n@gmail.com>
2022-12-04 10:00:35 -0300

    Primeiro commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+console.log("FUNFO")
+let imagem = document.querySelector(".starbucks")
+let circulo = document.querySelector(".circulo")
+
+function trocaImagem(endereco) {
+    imagem.src = endereco
+}
+function trocaCor(cor){
+    circulo.style.background = cor
+}
